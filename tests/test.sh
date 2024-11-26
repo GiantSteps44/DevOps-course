18fb05452cc21c0f60ed15d8c319bd457b045b9b
AriKu <ari.kukkaro@tuni.fi>
2024-11-26 19:05:12 +0200

    Testing pipeline added.

--- tests/test.sh
+++ tests/test.sh
@@ -1,1 +1,20 @@
-hurl --test *.hurl
+#!/bin/sh
+set -eu
+
+wait_for_url () {
+    echo "Testing $1..."
+    printf 'GET %s\nHTTP 200' "$1" | hurl --retry "$2" > /dev/null;
+    return 0
+}
+
+echo "Starting container"
+docker run --name testing --rm --detach --publish 8197:8197 ghcr.io/jcamiel/hurl-express-tutorial:latest
+
+echo "Waiting server to be ready"
+wait_for_url "$1" 60
+
+echo "Running Hurl tests"
+hurl --variable host="$1" --test tests/*.hurl
+
+echo "Stopping container"
+docker stop testing
