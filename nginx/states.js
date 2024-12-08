5bab2d17c8579bfd1ae165989126873360808686
AriKu <ari.kukkaro@tuni.fi>
2024-12-08 15:31:33 +0200

    State requests and run-log updated.

--- nginx/states.js
+++ nginx/states.js
@@ -2,39 +2,59 @@
 // define possible states
 var allowedStates = ['INIT','RUNNING','SHUTDOWN','PAUSED']
 
+  function setInitialState(){
+    ngx.shared.my_zone.set('currentState', 'INIT');
+}
+  function stateTransition(){
+    const now = new Date();
+    const isoString = now.toISOString();
+    let log = isoString + ": " + ngx.shared.my_zone.get('previousState') + "->" + ngx.shared.my_zone.get('currentState');
+    let runLog = ngx.shared.my_zone.get('runLog');
+    if (runLog) {
+        var runArr = runLog.split(",");
+    } else {
+        var runArr = [];
+    }
+    runArr.push(log);
+    const LogStr = runArr.toString();
+    ngx.shared.my_zone.set('runLog',LogStr);
 
+}
 
 // Handle GET and PUT requests
-function handleState(r) {
-    // In-memory store for previous and current states 
-    let state = ngx.shared.my_zone.get('currentState');
-    if (state == undefined) {
-        ngx.shared.my_zone.set('currentState', 'INIT');
-        state = ngx.shared.my_zone.get('currentState');
-        ngx.shared.my_zone.set('previousState',"None"); 
-    } else {
-    }
+  function handleState(r) {
     
+    // If initial state is not set, then set it
+    if (ngx.shared.my_zone.get('currentState') == undefined) {
+        setInitialState();
+        
+    }
+    var state = ngx.shared.my_zone.get('currentState');
+
+
     if (r.method === 'GET') {
-        // Handle GET request: Return the current value of the variable
+        // Handle GET request: Return the current value of state
         r.return(200, state + "\n");
     
+
     } else if (r.method === 'PUT') {
         try {
-            ngx.shared.my_zone.set('previousState',state); 
-            // Handle PUT request: Update the variable with request payload
-            state = r.requestText;
-                // Check if the body is in an allowed state
-            if (allowedStates.includes(state)) {
+            // Fetch new state from PUT request
+            var putState = r.requestText;
 
-                ngx.shared.my_zone.set('currentState', state);
-                
-                r.return(200, "State updated to: " + state + "\n");
-            } else{
+                // Check if new state is allowed
+            if (allowedStates.includes(putState)) {
+                // if so, then update previous state and current state
+                ngx.shared.my_zone.set('previousState',state)
+                ngx.shared.my_zone.set('currentState', putState);
+                stateTransition();
+                r.return(200, "State updated to: " + putState + "\n");
+            } else {
                 r.return(405, "Improper state\n");
             }
+
         } catch (error) {
-            r.return(405, "Improper state\n");
+            r.return(405, "Error: Improper state "+error.message +"\n");
         }
         
     } else {
@@ -44,15 +64,22 @@ function handleState(r) {
 }
   function getRunLog(r) {
     if (r.method === 'GET') {
-        const now = new Date();
-        const isoString = now.toISOString();
-        let runLog = isoString + ": " + ngx.shared.my_zone.get('previousState') + "->" + ngx.shared.my_zone.get('currentState');
-        ngx.shared.my_zone.set('dayTime',runLog);
-        let data = ngx.shared.my_zone.get('dayTime');
-        r.return(200,  data + "\n");
+        // if there is previous state (and current state)
+        // but runLog is undefined then add state
+        let runLog = ngx.shared.my_zone.get('runLog');
+        if (runLog){
+            var runArr = runLog.split(",")
+            r.return(200,  runArr.join("\n") + "\n");
+        } else {
+            r.return(200,  "No state transitions occurred." + "\n");
+        }
+        
+        
+
     } else {
         // Handle unsupported methods
         r.return(405, "Method Not Allowed\n");
     }
   }
-export default { handleState, getRunLog }
+
+export default { handleState, getRunLog, setInitialState }
