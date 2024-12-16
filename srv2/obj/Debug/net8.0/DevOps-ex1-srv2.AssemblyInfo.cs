543361f570eb01c4f89b6d15407f0c1d8ec80e4c
AriKu <ari.kukkaro@tuni.fi>
2024-12-16 13:50:04 +0200

    deployment files added.

--- srv2/obj/Debug/net8.0/DevOps-ex1-srv2.AssemblyInfo.cs
+++ srv2/obj/Debug/net8.0/DevOps-ex1-srv2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevOps-ex1-srv2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3792ff11f76bf033a89dc11e32883b7df3336005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a4f2ff50c376a3bc28c59e25a05fa5c74daa75a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevOps-ex1-srv2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevOps-ex1-srv2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
